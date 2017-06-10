9cef30f4fc1462b8f4879214e6f5dffa3c3dd86c
Administrador <Administrador@10.10.3.7>
2017-06-10 09:02:14 -0300

    Primeira versao

--- Aula10062017_conexaoBanco/src/aula10062017_conexaobanco/Aula10062017_conexaoBanco.java
+++ Aula10062017_conexaoBanco/src/aula10062017_conexaobanco/Aula10062017_conexaoBanco.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aula10062017_conexaobanco;
 
-/**
- *
- * @author Administrador
- */
+
 public class Aula10062017_conexaoBanco {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ola mundo!");
     }
     
 }
